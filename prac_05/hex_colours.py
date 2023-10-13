535b6436072d7cb6574add7b91d5d73bafa3db42
Luke-hawkins <145887513+Luke-hawkins@users.noreply.github.com>
2023-10-13 22:57:56 +1000

    added dictionary colour_to_hex to hex_colours

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,24 @@
+def main():
+    colour_to_hex = {"Absolute Zero": "#0048ba",
+                     "Acid Green": "#b0bf1a",
+                     "Alizarin crimson": "#e32636",
+                     "Amaranth": "#e52b50",
+                     "Amber": "#ffbf00",
+                     "Amethyst": "#9966cc",
+                     "AntiqueWhite": "#faebd7",
+                     "Azure3": "#c1cdcd",
+                     "Azure4": "#838b8b",
+                     "Baby Blue": "#89cff0",
+                     "Baby Pink": "#f4c2c2",
+                     "Baker-Miller Pink": "#ff91af",
+                     "Banana Mania": "#fae7b5",
+                     "Banana Yellow": "#ffe135",
+                     "Barn Red": "#7c0a02"}
+    colour = input("colour: ")
+    while colour != "":
+        try:
+            colour in
+
+
+
+main()
