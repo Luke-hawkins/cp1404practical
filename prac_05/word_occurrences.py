7724541e260eab786eff2edae5d39bba48a4a32a
Luke-hawkins <145887513+Luke-hawkins@users.noreply.github.com>
2023-10-13 23:37:14 +1000

    completed prac 5 word occurrences

---
+++ prac_05/word_occurrences.py
@@ -0,0 +1,26 @@
+"""
+Word Occurrences
+Estimate: 30 minutes
+Actual:   15 minutes
+"""
+
+
+def main():
+    """counts the occurrences of each word in a string"""
+    word_to_count = {}
+    text = input("Text: ").split()
+    for word in text:
+        if word in word_to_count:
+            word_to_count[word] = word_to_count[word] + 1
+        else:
+            word_to_count[word] = 1
+
+    print_word_count(word_to_count)
+
+
+def print_word_count(word_to_count):
+    """Displays sorted word occurrences"""
+    [print(word + " : " + str(word_to_count[word])) for word in sorted(word_to_count)]
+
+
+main()
