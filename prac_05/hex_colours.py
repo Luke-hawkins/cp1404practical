66e8e27e99acb463518675622115e771752f8672
Luke-hawkins <145887513+Luke-hawkins@users.noreply.github.com>
2023-10-13 23:08:32 +1000

    completed prac 5 hex-colour

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -1,24 +1,21 @@
 def main():
-    colour_to_hex = {"Absolute Zero": "#0048ba",
-                     "Acid Green": "#b0bf1a",
-                     "Alizarin crimson": "#e32636",
-                     "Amaranth": "#e52b50",
-                     "Amber": "#ffbf00",
-                     "Amethyst": "#9966cc",
-                     "AntiqueWhite": "#faebd7",
-                     "Azure3": "#c1cdcd",
-                     "Azure4": "#838b8b",
-                     "Baby Blue": "#89cff0",
-                     "Baby Pink": "#f4c2c2",
-                     "Baker-Miller Pink": "#ff91af",
-                     "Banana Mania": "#fae7b5",
-                     "Banana Yellow": "#ffe135",
-                     "Barn Red": "#7c0a02"}
-    colour = input("colour: ")
+    """displays Hex code based of users colour input"""
+    colour_to_hex = {"Absolute Zero": "#0048ba", "Acid Green": "#b0bf1a",
+                     "Alizarin crimson": "#e32636", "Amaranth": "#e52b50",
+                     "Amber": "#ffbf00", "Amethyst": "#9966cc",
+                     "AntiqueWhite": "#faebd7", "Azure3": "#c1cdcd",
+                     "Azure4": "#838b8b", "Baby Blue": "#89cff0",
+                     "Baby Pink": "#f4c2c2", "Baker-Miller Pink": "#ff91af",
+                     "Banana Mania": "#fae7b5", "Banana Yellow": "#ffe135",
+                     "Barn Red": "#7c0a02", "Battleship Gray": "#848482"}
+
+    colour = input("colour: ").title()
     while colour != "":
         try:
-            colour in
-
+            print(colour, "hexadecimal colour code: ", colour_to_hex[colour])
+        except KeyError:
+            print("no")
+        colour = input("colour: ").title()
 
 
 main()
